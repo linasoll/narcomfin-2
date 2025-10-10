5eaf8da906bc371ec9eb8234ae1170dfa93784d7
Angelina S <soldatenko_angelina@mail.ru>
2025-10-10 18:17:45 +0400

    homework 11 oct

---
+++ javascripts/sidebar.js
@@ -0,0 +1,12 @@
+$(function() { 
+    $('.sidebar a').each(function() { 
+        $(this).on('click', function(e) { 
+            e.preventDefault(); 
+            var scrollTo = $(this).attr('href'); 
+            var whitespace = scrollTo == '#1' ? 150 : 110;
+            $('html,body').animate({ 
+                scrollTop: $(scrollTo).offset().top - whitespace 
+            }, 1000);
+        }); 
+    });
+});
